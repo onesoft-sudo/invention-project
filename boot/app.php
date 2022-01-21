cbc28a7a3c263aac5567dee57b03bcb782daacbe
Ar Rakin <rakinar2@gmail.com>
2022-01-21 16:31:45 +0600

    Fixed minor bugs

---
+++ boot/app.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Date: 01/16/2022 11:35 PM
+ * @author Ar Rakin <rakinar2@gmail.com>
+ */
---
+++ resources/views/api/index.power.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Date: 01/17/2022 11:01 PM
+ * @author Ar Rakin <rakinar2@gmail.com>
+ */
