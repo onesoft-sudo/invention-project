422c2825d6c084f171c62d9677997aaad6685da1
Ar Rakin <rakinar2@gmail.com>
2022-03-13 16:37:05 +0600

    Update bindings.php

--- config/bindings.php
+++ config/bindings.php
@@ -1,9 +1,5 @@
 <?php
 
 return [
-    \App\PaymentGatewayContract::class => [
-        'object' => new \App\CreditPaymentGateway('INR'),
-        'once' => true,
-        'prop' => 'payment'
-    ]
+    
 ];
