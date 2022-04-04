4099b12051ec8096968634b4026c9f9966e45420
Ar Rakin <rakinar2@gmail.com>
2022-04-04 19:33:35 +0600

    Update VerifyCSRF.php

--- app/Http/Middleware/VerifyCSRF.php
+++ app/Http/Middleware/VerifyCSRF.php
@@ -9,29 +9,21 @@ use OSN\Framework\Http\Request;
 
 class VerifyCSRF extends Middleware
 {
-    use CSRFHelper;
-
     /**
      * @return ?bool
      * @throws HTTPException
      */
     public function handle(Request $request)
     {
-        /**
-        *   Return true or null to evaluate the request as OK. Throw
-        *   an App\Exceptions\HTTPException for rejecting it and
-        *   rendering an error page. You can also return a View or
-        *   Response object here.
-        */
         if ($request->isWriteRequest() && !test_env()) {
-            $csrf_token = self::get();
             $csrf_request = $request->post("__csrf_token");
 
-            if (!$csrf_token || !$csrf_request || $csrf_token != $csrf_request) {
+            if (!$csrf_request || !app()->csrf->isValid($csrf_request)) {
+                app()->csrf->endCSRF();
                 throw new HTTPException(419, "Page Expired");
             }
 
-            self::endCSRF();
+            app()->csrf->prune();
         }
 
         return true;
